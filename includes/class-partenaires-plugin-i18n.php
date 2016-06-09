c94373a55f92db5a1826fad81ce49eee4c14769a
Yoan marchal <marchalyoan@gmail.com>
2016-06-09 21:55:24 +0200

    correct file name

--- includes/class-partenaires-plugin-i18n.php
+++ includes/class-partenaires-plugin-i18n.php
@@ -30,7 +30,7 @@ class partenaires_plugin_i18n
     public function load_plugin_textdomain()
     {
         load_plugin_textdomain(
-            'slider-plugin',
+            'partenaires-plugin',
             false,
             dirname(dirname(plugin_basename(__FILE__))).'/languages/'
         );
